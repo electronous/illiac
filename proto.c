1b8624414572fa00611121d161d66cd7b40770a5
Brian Armstrong <brian@brian-laptop.(none)>
2010-02-20 13:50:15 -0600

    Started memory access.

--- proto.c
+++ proto.c
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+byte_t memory;
+
 typedef struct {
 	uint8_t data;
 	bool flag;
@@ -30,26 +32,11 @@ typedef struct {
 	hw_t start_page;
 } base_pointer_t;
 	
+typedef int raw_address_t;
 
 typedef struct {
-	number_format_t pr_0;
-	number_format_t pr_1;
-	number_format_t pr_2;
-	number_format_t pr_3;
-	number_format_t pr_4;
-	number_format_t pr_5;
-	number_format_t pr_6;
-	number_format_t pr_7;
-	number_format_t pr_8;
-	number_format_t pr_9;
-	number_format_t pr_10;
-	number_format_t pr_11;
-	number_format_t pr_12;
-	number_format_t pr_13;
-	base_pointer_t br_0;
-	base_pointer_t br_1;
-	base_pointer_t br_2;
-	base_pointer_t br_3;
+	number_format_t pr[14];
+	base_pointer_t br[6];
 	asf_t pr_14;
 } cpu_t;
 
@@ -58,11 +45,49 @@ exec(nf_t instruction_word) {
 	return;
 }
 
+raw_address_t get_address_common(number_format_t *p, cpu_t *cpu) {
+    uint8_t br_num;
+    br_num = (p->pointer_link.low.flag << 2);
+    br_num += (p->pointer_value.high.flag << 1);
+    br_num += (p->pointer_value.low.flag);
+
+    return (raw_address_t)(cpu->br[br_num].start_page.data << 8);
+
+}
+
+raw_address_t get_address_from_pointer(number_format_t *p, cpu_t *cpu) {
+    raw_address_t ret;
+    ret = get_address_common(p, cpu);
+    ret += (p->pointer_value.high.data << 8);
+    ret += (p->pointer_value.low.data);
+
+    return ret; 
+}
+
+raw_address_t get_address_from_link(number_format_t *p, cpu_t *cpu) {
+    raw_address_t ret;
+    ret = get_address_common(p, cpu);
+    ret += (p->pointer_link.high.data << 8);
+    ret += (p->pointer_link.low.data);
+
+    return ret;
+}
+
+
 int main(void) {
 	cpu_t cpu;
+    memory = malloc(sizeof(byte_t) * (1 << 24));
+    if(!memory) {
+        printf("oh god help!!");
+        exit(-1);
+    }
 	for (;;) {
-		nf_t current_instruction = cpu.pr_0;
-		cpu.pr_0 = mem(cpu.pr_0.pointer_link);
+        raw_address_t next;
+		nf_t current_instruction = cpu.pr[0];
+        if((next = get_address_from_link(&(cpu.pr[0]), &cpu) == -1)) {
+                printf("Segfault.");
+        }
+		cpu.pr[0] = memory[next];
 		exec(mem(current_instruction.pointer_value));
 	}
 	return 0;
