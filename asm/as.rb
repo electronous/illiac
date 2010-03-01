ce367aef028e31ff14680243fedb7e01380e6517
David Majnemer <majnemer@apple.com>
2010-03-01 09:10:14 +0000

    new asm writer

--- asm/as.rb
+++ asm/as.rb
@@ -5,24 +5,14 @@ require 'bytewriter'
 infile  = ARGV[0]
 outfile = ARGV[1]
 
-if infile == nil:
-    abort("You must specify an input asm file")
+if infile.nil?:
+	abort("You must specify an input asm file")
 end
 
-if outfile == nil:
-    abort("You must specify an output bin file")
+if outfile.nil?:
+	abort("You must specify an output bin file")
 end
 
-ByteWriter.new(outfile).go do |writer|
-    File.open(infile, "r").each do |line|
-        instruction = line.split()
-        opcode      = instruction[0]
-        operands    = instruction[1..-1]
+load infile
 
-        if not writer.respond_to?(opcode):
-            abort("Invalid opcode: " + opcode)
-        end
-
-        writer.send(opcode, *operands)
-    end
-end
+ByteWriter.instance.write_out(outfile)
--- asm/bytewriter.rb
+++ asm/bytewriter.rb
@@ -1,33 +1,86 @@
 require 'rubygems'
 require 'bindata'
+require 'singleton'
 
-class ZeroOperand < BinData::Record
-    endian :little
+class ZeroOperandInst < BinData::Record
+	endian :little
 
-    uint8  :opcode
-    bit1   :flag
+	uint8 :opcode
+	bit1  :flag
 end
 
 class ByteWriter
-    def initialize(outfile)
-        @outfile = outfile
-    end
+	include Singleton
 
-    def DUPB
-        new_op = ZeroOperand.new
-        new_op.opcode = 0b00100100
-        new_op.flag   = 1
-        @instructions.push(new_op)
-    end
+	def initialize
+		@instructions = []
+	end
 
-    def go(&block)
-        @instructions = Array.new
-        File.open(@outfile, "w") do |@fh|
-            yield self
+	def add_instruction(instruction)
+		@instructions << instruction
+	end
 
-            @instructions.each do |instruction|
-                @fh.write(instruction.to_binary_s)
-            end
-        end
-    end
+	def write_out(outfile)
+		File.open(outfile, "w") do |fh|
+			@instructions.each do |instruction|
+				fh.write(instruction.to_binary_s)
+			end
+		end
+	end
+end
+
+def abs_short
+	new_op = ZeroOperandInst.new
+	new_op.opcode = 0b10010100
+	new_op.flag   = 1
+	ByteWriter.instance.add_instruction(new_op)
+end
+
+def abs_long
+	new_op = ZeroOperandInst.new
+	new_op.opcode = 0b10010101
+	new_op.flag   = 1
+	ByteWriter.instance.add_instruction(new_op)
+end
+
+def dupb
+	new_op = ZeroOperandInst.new
+	new_op.opcode = 0b00100100
+	new_op.flag   = 0
+	ByteWriter.instance.add_instruction(new_op)
+end
+
+def duphw
+	new_op = ZeroOperandInst.new
+	new_op.opcode = 0b00100101
+	new_op.flag   = 0
+	ByteWriter.instance.add_instruction(new_op)
+end
+
+def dupw
+	new_op = ZeroOperandInst.new
+	new_op.opcode = 0b00100110
+	new_op.flag   = 0
+	ByteWriter.instance.add_instruction(new_op)
+end
+
+def sluffb
+	new_op = ZeroOperandInst.new
+	new_op.opcode = 0b00100000
+	new_op.flag   = 0
+	ByteWriter.instance.add_instruction(new_op)
+end
+
+def sluffhw
+	new_op = ZeroOperandInst.new
+	new_op.opcode = 0b00100001
+	new_op.flag   = 0
+	ByteWriter.instance.add_instruction(new_op)
+end
+
+def sluffw
+	new_op = ZeroOperandInst.new
+	new_op.opcode = 0b00100001
+	new_op.flag   = 0
+	ByteWriter.instance.add_instruction(new_op)
 end
