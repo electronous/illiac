fdd98fd6181ed888f8a35d759510d8dcf4bf4e7f
David Majnemer <majnemer@apple.com>
2010-03-02 07:35:45 +0000

    get rid of colon

--- asm/as.rb
+++ asm/as.rb
@@ -5,11 +5,11 @@ require 'bytewriter'
 infile  = ARGV[0]
 outfile = ARGV[1]
 
-if infile.nil?:
+if infile.nil?
 	abort("You must specify an input asm file")
 end
 
-if outfile.nil?:
+if outfile.nil?
 	abort("You must specify an output bin file")
 end
 
